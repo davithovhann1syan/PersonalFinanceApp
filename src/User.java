076328018347a3e8e2ec19ca714b726913eb418a
Davit Hovhannisyan <davit05hovhannisyan@gmail.com>
2024-03-19 17:02:24 +0400

    User class added

---
+++ src/User.java
@@ -0,0 +1,14 @@
+public class User {
+    private int id;
+    private String username;
+    private String password;
+
+
+    public boolean login(String username, String password) {
+        return true;
+    }
+
+    public void logout() {
+
+    }
+}
